baadd7192fe9885db5e3df20227e5d8f152427a3
Sean McDonald <stmcdon19@gmail.com>
2020-11-20 09:17:51 -0500

    created Game class. Player can click on choice buttons to play a round. Results are displayed on screen.

--- script.js
+++ script.js
@@ -1,29 +1,83 @@
+class Game {
+  //fields
+  score = {
+    user: 0,
+    cpu: 0
+  };
+  userChoice;
+  // cpuChoice;
+  rounds;
+
+  //constructor
+  constructor(rounds){
+    this.rounds = rounds;
+  };
+
+  //getters
 
 
+  //setters
 
 
+  //methods
+  makeCpuChoice(){
+    let choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
+  };
 
+  chooseWinner(userChoice, cpuChoice){
+    let whatBeatsIt = {
+      rock: "paper",
+      paper: "scissors",
+      scissors: "rock"
+    };
+    if (cpuChoice === userChoice) {
+      return 'tie';
+    } else if (whatBeatsIt[userChoice] === cpuChoice){
+      return 'cpu';
+    } else {
+      return 'user';
+    };
+  };
 
+  playRound(userChoice){
+    let cpuChoice = this.makeCpuChoice();
+    let winner = this.chooseWinner(userChoice, cpuChoice);
+    console.log(cpuChoice);
+    console.log(winner);
+    if (winner === 'cpu'){
+      this.score['cpu']++;
+    } else if (winner === 'user') {
+      this.score['user']++;
+    };
+    console.log(this.score);
+    return [cpuChoice, winner]
+  };
+  playGame(rounds){};
+}
 
-
-
-
-
-
-
+//initialize game
+game = new Game(3);
 
 //Take a choice from the user using event handler
 const choiceButtons = document.querySelectorAll(".choice");
 choiceButtons.forEach(el => {
   el.addEventListener('click', function() {
-    let userSelection = this.id;
-    makeChoice(userSelection);
+    let userChoice = this.id;
+    let [cpuChoice, winner] = game.playRound(userChoice);
+    if (winner === 'tie') {
+      setMessage(`You chose ${userChoice}. The CPU chose ${cpuChoice}. It's a tie, go again!`)
+    } else if (winner === 'cpu'){
+      setMessage(`You chose ${userChoice}. The CPU chose ${cpuChoice}. You lost! Choose the right one next time.`)
+    } else if (winner === 'user'){
+      setMessage(`You chose ${userChoice}. The CPU chose ${cpuChoice}. You won! Congrats, have a cookie.`)
+    }
   });
 });
 
 
-function makeChoice(choice) {
-  document.querySelector('#current-msg').textContent = `You have chosen ${choice}`;
+function setMessage(message) {
+  document.querySelector('#current-msg').textContent = message;
 }
 
 
